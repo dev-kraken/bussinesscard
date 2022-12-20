0ab5b9ab84effae90a1f2804f1f40af5fe6dd80a
Dev Kraken <devkraken@Dev.Kraken>
2022-12-20 05:32:22 +0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,169 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image';
+import banner from "./123.jpg";
+import facebook from "../icons/PNG/FrontView/Facebook.png";
+import messenger from "../icons/PNG/FrontView/Messenger.png";
+import instagram from "../icons/PNG/FrontView/Instagram.png";
+import linkedin from "../icons/PNG/FrontView/LinkedIn.png";
+import discord from "./profile.webp";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+    <div className="container max-w-md mx-auto font-poppins">
+      {/* Banner  */}
+      <Image
+        src={banner}
+        alt="Picture of the author"
+      /> 
+
+      <Image className="bg-amber-100 rounded-full w-36 mx-auto -mt-16"
+        src={discord}
+        alt="Picture of the author"
+      />
+
+      {/* Title Header  */}
+      <h4 className="font-mono text-center">Real Estate</h4>
+      {/* Social Media Icons  */}
+      <div className="gap-8 columns-4 mx-auto px-5">
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+          <a href="">
+          <Image className="hover:bg-sky-700"
+            src={facebook}
+            alt="Picture of the author"
+          />
+          <h5>Facebook </h5>
           </a>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div>
+          <Image
+            src={messenger}
+            alt="Picture of the author"
+          />
+          <h5>Messenger</h5>
+
+        </div>
+
+        <div>
+          <Image
+            src={instagram}
+            alt="Picture of the author"
+          />
+          <h5>Instagram</h5>
+
+        </div>
+
+
+        <div>
+          <Image
+            src={linkedin}
+            alt="Picture of the author"
+          />
+          <h5>Linkedin</h5>
+
+        </div>
+      </div>
+      <div className="gap-8 columns-4 mx-auto px-5">
+        <div>
+          <Image
+            src={facebook}
+            alt="Picture of the author"
+          />
+          <h5>Facebook</h5>
+        </div>
+
+        <div>
+          <Image
+            src={messenger}
+            alt="Picture of the author"
+          />
+          <h5>Messenger</h5>
+
+        </div>
+
+        <div>
+          <Image
+            src={instagram}
+            alt="Picture of the author"
+          />
+          <h5>Instagram</h5>
+
+        </div>
+
+
+        <div>
+          <Image
+            src={linkedin}
+            alt="Picture of the author"
+          />
+          <h5>Linkedin</h5>
+
+        </div>
+      </div>
+      <div className="gap-8 columns-4 mx-auto px-5">
+        <div>
+          <Image
+            src={facebook}
+            alt="Picture of the author"
+          />
+          <h5>Facebook</h5>
+        </div>
+
+        <div>
+          <Image
+            src={messenger}
+            alt="Picture of the author"
+          />
+          <h5>Messenger</h5>
+
+        </div>
+
+        <div>
+          <Image
+            src={instagram}
+            alt="Picture of the author"
+          />
+          <h5>Instagram</h5>
+
+        </div>
+
+
+        <div>
+          <Image
+            src={linkedin}
+            alt="Picture of the author"
+          />
+          <h5>Linkedin</h5>
+
         </div>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* About */}
+      <h1 className="text-3xl mt-4 text-center font-sans"> About </h1>
+      <p className="p-3 text-center">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
+      {/* Contant */}
+      <h1 className="text-3xl mt-4 text-center font-sans"> Contant Me </h1>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <form className="grid p-5" action="/send-data-here" method="post">
+        <label htmlFor="first">First name:</label>
+        <input className="bg-sky-100 focus:outline-none p-2" type="text" id="first" name="first" />
+        <label htmlFor="last">Last name:</label>
+        <input className="bg-sky-100 focus:outline-none p-2" type="text" id="last" name="last" />
+        <label htmlFor="email">Email:</label>
+        <input className="bg-sky-100 focus:outline-none p-2" type="email" id="email" name="email" />
+        <label htmlFor="message">Message:</label>
+        <textarea className="bg-sky-100 focus:outline-none p-2" id="message" name="message" />
+      </form>
+
+
+      {/* Footer  */}
+
+      <footer className="p-4 bg-white  md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+        <div className="text-center mx-auto text-white">Business Card</div>
+      </footer>
+
+    </div>
   )
 }
